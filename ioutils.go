7b34dfcf3aad24ee863a3690c20d0cd02b398047
Peter Freese <freese.peter@gmail.com>
2019-01-13 11:00:19 -0800

    reorganize

---
+++ ioutils.go
@@ -0,0 +1,13 @@
+package rbns
+
+import "os"
+
+// CreateDirIfNotExist creates the dir if it does not already exist
+func CreateDirIfNotExist(dir string) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
